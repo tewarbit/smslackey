c99df913fec475611e94dc0b0b730951cc4b80df
btewari <btewari@gmail.com>
2012-11-29 19:08:19 -0500

    implement movie time rendering

---
+++ config.py
@@ -0,0 +1,6 @@
+
+twilio = {
+	'account': "<enter twilio account here>",
+	'token': "<enter twilio token here>",
+	'number': "<twilio number>"
+}
--- smsrequesthandler.py
+++ smsrequesthandler.py
@@ -5,6 +5,7 @@ import re
 import StringIO
 import lxml.html
 from google.appengine.api import urlfetch
+from movietimerenderer import MovieTimeRenderer
 
 class SmsRequestHandler(webapp2.RequestHandler):
 	def get_movie_times(self):
@@ -31,32 +32,55 @@ class SmsRequestHandler(webapp2.RequestHandler):
 		logging.info('Found ' + str(len(movie_nodes)) + ' movies')
 		for movie in movie_nodes:
 			movie_name = movie.xpath('.//div[@class="name"]/a')[0].text
-			logging.info("Finding times for movie name: " + movie_name)
 			unicode_movie_times = re.findall(r"\d{1,2}:\d\d[amp]{0,2}", movie.text_content())
 			movie_times = map(lambda uni: uni.encode('ascii', 'ignore'), unicode_movie_times)
 			movies.append((movie_name, movie_times))
 		
 		return movies
+	
+	def filter_movies(self, movie_list, query):
+		query_words = self.words(query.lower())
+		three_d_query = '3d' in query_words
+		imax_query = 'imax' in query_words
+		
+		def movie_filter(movie_title):
+			movie_words = self.words(movie_title[0].lower())
+			
+			logging.info("Looking for query_words: " + str(query_words) + " in movie_words: " + str(movie_words))
+			queried_movie = all(query_word in movie_words for query_word in query_words)
+			if not queried_movie: return False
+
+			three_d_movie = '3d' in movie_words
+			imax_movie = 'imax' in movie_words
+			logging.info("3d query: " + str(three_d_query) + " imax query: " + str(imax_query))
+			logging.info("3d movie: " + str(three_d_movie) + " imax movie: " + str(imax_movie))
+			return three_d_query == three_d_movie and imax_query == imax_movie
+			
+		filtered_movies = filter(movie_filter, movie_list)
+		return filtered_movies
+		
 		
 	def filter_out_movies(self, movie_infos, query):
-		def filter_fn(val): return any((query.lower() in re.findall(r"[\w']+", item[0].lower())) for item in val)
+		#logging.info("filtering out movies from: " + str(movie_infos))
+		query_words = self.words(query.lower())
 		
-		def filter_movies(movie_list):
-			return [item for item in movie_list if query.lower() in re.findall(r"[\w']+", item[0].lower())]
+		def filter_fn(val): return any(all(query_word in movie_title[0].lower() for query_word in query_words) for movie_title in val)
 		
-		return collections.OrderedDict((k, filter_movies(v)) for k, v in movie_infos.iteritems() if filter_fn(v))
+		return collections.OrderedDict((k, self.filter_movies(v, query)) for k, v in movie_infos.iteritems() if filter_fn(v))
+		
+	def words(self, sentence): return re.findall(r"[\w']+", sentence)
 	
 	def post(self):
 		logging.info('Handling a post request')
 		movie_times = self.get_movie_times()
 		movie_times = self.filter_out_movies(movie_times, self.request.get('Body'))
-		logging.info(movie_times)
+		# logging.info(movie_times)
 		self.response.out.write('<html><body>')
 		self.response.out.write('got message from: ' + self.request.get('From') + '<br/>')
 		self.response.out.write(' with contents: ' + self.request.get('Body') + '<br/>')
 		
-		for k in movie_times.keys()[0:3]:
-			self.response.out.write('<b>' + k + '</b>' + ' ' + str(movie_times[k]) + ' <br/>')
+		self.response.out.write(MovieTimeRenderer.render(movie_times))
+
 		self.response.out.write('</pre></body></html>')
 
 	def get(self):
